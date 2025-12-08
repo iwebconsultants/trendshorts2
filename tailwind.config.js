b1004084ff623abc40e4553dd3ce71479e2d351e
Azam <iotwise.io@gmail.com>
2025-12-08 12:40:12 +1100

    Fix: Tailwind config for root-level files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./index.tsx",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./types.ts" // In case there are classes in types (unlikely but safe)
+    "./services/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
